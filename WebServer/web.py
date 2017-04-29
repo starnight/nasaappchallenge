86464fcd1308715a9bce0d7b38dc011b36be2cd6
StarNight <zack_pan@alumni.ncu.edu.tw>
2017-04-29 22:32:15 +0800

    Add Air Temperature, Tide, Wind, Escherichia coli, Under Sea Visibility web API.

--- WebServer/web.py
+++ WebServer/web.py
@@ -25,6 +25,10 @@ def getChlorophyll(date, lon, lat):
 def getOceanSurfaceTemp(date, lon, lat):
 	return get_ocean_surface_temp(date,lat,lon)
 
+@route('/GetAirTemp/<date>/<lon:float>/<lat:float>')
+def getAirTemp(date, lon, lat):
+	return data[date][lon][lat]
+
 @route('/GetSolarInsolation/<date>/<lon:float>/<lat:float>')
 def getSolarInsolation(date, lon, lat):
 	return get_solar_insolation(date,lat,lon)
@@ -32,6 +36,23 @@ def getSolarInsolation(date, lon, lat):
 @route('/GetUV/<date>/<lon:float>/<lat:float>')
 def getUV(date, lon, lat):
 	return data[date][lon][lat]
+
+@route('/GetTide/<date>/<lon:float>/<lat:float>')
+def getTide(date, lon, lat):
+	return data[date][lon][lat]
+
+@route('/GetWind/<date>/<lon:float>/<lat:float>')
+def getWind(date, lon, lat):
+	return data[date][lon][lat]
+
+@route('/GetEscherichiacoli/<date>/<lon:float>/<lat:float>')
+def getEscherichiacoli(date, lon, lat):
+	return data[date][lon][lat]
+
+@route('/GetUnderSeaVisibility/<date>/<lon:float>/<lat:float>')
+def getUnderSeaVisibility(date, lon, lat):
+	return data[date][lon][lat]
+
 
 if __name__ == "__main__":
 	# Build data sets
