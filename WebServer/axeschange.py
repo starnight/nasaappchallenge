b8efc9e85fa9ffb8cb54beca8d5a08874cc1565d
StarNight <zack_pan@alumni.ncu.edu.tw>
2017-04-29 12:29:06 +0800

    Add axis change.

---
+++ WebServer/axeschange.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+lon_domain = []
+lat_domain = []
+
+def haveLonDoamin():
+	for i in range(-1800, 1800):
+		lon_domain.append(i)
+
+def haveLatDomain():
+	for i in range(-900, 900):
+		lat_domain.append(-i)
+
+haveLonDoamin()
+haveLatDomain()
+
+def lon2col(lon):
+	return lon_domain.index(round(lon*10))
+
+def lat2row(lat):
+	return lat_domain.index(round(lat*10))
+
+def lonlat2arridx(lon, lat):
+	return lon2col(lon), lat2row(lat)
